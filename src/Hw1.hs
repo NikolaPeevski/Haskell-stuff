629fd49fb902a6d78f70fea1e259c6089af27a27
nikolapeevski <Jeratull@hotmail.com>
2020-01-30 20:09:14 -0600

    Refactor HW1 Add comments

--- src/Hw1.hs
+++ src/Hw1.hs
@@ -13,7 +13,7 @@ dft :: [Double] -> [(Double, Double)]
 sinSum :: [Double] -> Double -> Double -> Double -> Double
 cosSum :: [Double] -> Double -> Double -> Double -> Double
 
-
+-- Range implementation using tail recursion
 range from to count =
     innerRange from to count (count-1)
     where
@@ -23,25 +23,30 @@ range from to count =
             in
                  innerRange a b c (d-1) ++ [a + d * step]
 
+-- Round implementation using pattern matching
 rd x [] = []
 rd x (a:b) = fromIntegral (round (a * z)) / z : rd x b
     where z = 10^x
 
+-- Absolute implementation using map
 absolute = map (\ a -> sqrt (fst a * fst a + snd a * snd a))
 
+-- Helper sin function
 sinSum [] _ _ _ = 0
 sinSum (a:b) k n 0 = a * sin((2 * pi * k * 0)/ n)
 sinSum (a:b) k n n' = a * sin((2 * pi * k * n')/ n) + sinSum b k n (n' - 1)  
 
+-- Helper cos function
 cosSum [] _ _ _ = 0
 cosSum (a:b) k n 0 = a * cos((2 * pi * k * 0)/ n)
 cosSum (a:b) k n n' = a * cos((2 * pi * k * n')/ n) + cosSum b k n (n' - 1)  
     
 
+-- Discrete Fourier Transform implementation using helper functions and tail recursion
 dft x =
     innerDft x (fromIntegral (length x)) (fromIntegral (length x))
     where
         innerDft _ (-1) _ = []
         innerDft x i l =
-            [(sinSum x i l l, cosSum x i l l)] ++ innerDft x (i-1) l
+            (sinSum x i l l, cosSum x i l l) : innerDft x (i-1) l
     
